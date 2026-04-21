1779979eea67db8f1fce3ed653c5c26884656917
GaskaPiotr <04piotr04@wp.pl>
2026-04-21 22:14:44 +0200

    chore: add wallet manager framework
    Adds wallet external and internal api
    Also wallet manager implements them

---
+++ src/main/java/com/github/gaskapiotr/stockmarketsim/wallet/WalletExternalAPI.java
@@ -0,0 +1,4 @@
+package com.github.gaskapiotr.stockmarketsim.wallet;
+
+public interface WalletExternalAPI {
+}
--- src/main/java/com/github/gaskapiotr/stockmarketsim/wallet/manager/WalletManager.java
+++ src/main/java/com/github/gaskapiotr/stockmarketsim/wallet/manager/WalletManager.java
@@ -1,7 +1,9 @@
 package com.github.gaskapiotr.stockmarketsim.wallet.manager;
 
+import com.github.gaskapiotr.stockmarketsim.wallet.WalletExternalAPI;
+import com.github.gaskapiotr.stockmarketsim.wallet.WalletInternalAPI;
 import org.springframework.stereotype.Service;
 
 @Service
-public class WalletManager {
+public class WalletManager implements WalletExternalAPI, WalletInternalAPI {
 }
