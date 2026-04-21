fc2ffae3716b4fd0bc136c99708beb58fbfc1e37
GaskaPiotr <04piotr04@wp.pl>
2026-04-21 22:55:47 +0200

    chore: add trade stock framework
    Adds trade stock framework
    Also adds trade request and trade type

--- src/main/java/com/github/gaskapiotr/stockmarketsim/gateway/GatewayManager.java
+++ src/main/java/com/github/gaskapiotr/stockmarketsim/gateway/GatewayManager.java
@@ -1,8 +1,24 @@
 package com.github.gaskapiotr.stockmarketsim.gateway;
 
+import com.github.gaskapiotr.stockmarketsim.gateway.request.TradeRequest;
+import com.github.gaskapiotr.stockmarketsim.gateway.request.TradeType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GatewayManager {
-
+    @PostMapping("/wallets/{wallet_id}/stocks/{stock_name}")
+    public void tradeStock(
+            @PathVariable String wallet_id,
+            @PathVariable String stock_name,
+            @RequestBody TradeRequest tradeRequest) {
+        if (tradeRequest.type() == TradeType.BUY) {
+            // TODO bank sells then user buys
+        } else if (tradeRequest.type() == TradeType.SELL) {
+            // TODO user sells then bank buys
+        }
+        // TODO return HTTP response
+    }
 }
---
+++ src/main/java/com/github/gaskapiotr/stockmarketsim/gateway/request/TradeType.java
@@ -0,0 +1,6 @@
+package com.github.gaskapiotr.stockmarketsim.gateway.request;
+
+public enum TradeType {
+    BUY,
+    SELL
+}
