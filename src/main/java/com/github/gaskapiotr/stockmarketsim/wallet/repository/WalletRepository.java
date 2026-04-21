03c58f587f56f25dcd9a051b21188066afcd3b15
GaskaPiotr <04piotr04@wp.pl>
2026-04-21 22:09:36 +0200

    fet: add wallet repo
    Adds wallet repo that extends jpa repo

--- src/main/java/com/github/gaskapiotr/stockmarketsim/wallet/repository/WalletRepository.java
+++ src/main/java/com/github/gaskapiotr/stockmarketsim/wallet/repository/WalletRepository.java
@@ -1,7 +1,9 @@
 package com.github.gaskapiotr.stockmarketsim.wallet.repository;
 
+import com.github.gaskapiotr.stockmarketsim.wallet.entity.Wallet;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class WalletRepository {
+public interface WalletRepository extends JpaRepository<Wallet, String> {
 }
