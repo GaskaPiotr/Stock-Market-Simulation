f9f42158cc0ab2239e3af109e694bce5625321c8
GaskaPiotr <04piotr04@wp.pl>
2026-04-21 22:07:11 +0200

    fet: add wallet and wallet stock entities
    Adds wallet with id and stocks
    Also adds wallet stock with id, name and quantity

--- src/main/java/com/github/gaskapiotr/stockmarketsim/wallet/entity/Wallet.java
+++ src/main/java/com/github/gaskapiotr/stockmarketsim/wallet/entity/Wallet.java
@@ -1,7 +1,19 @@
 package com.github.gaskapiotr.stockmarketsim.wallet.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Wallet {
+    @Id
+    private String id;
+    @OneToMany(mappedBy="wallet")
+    private List<WalletStock> stocks;
 }
