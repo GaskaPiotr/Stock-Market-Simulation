f599666771486498a7b70eb34341a003eb4cf609
GaskaPiotr <04piotr04@wp.pl>
2026-04-22 16:50:40 +0200

    fet: add bank entity to dto

--- src/main/java/com/github/gaskapiotr/stockmarketsim/bank/mapper/BankMapper.java
+++ src/main/java/com/github/gaskapiotr/stockmarketsim/bank/mapper/BankMapper.java
@@ -8,4 +8,5 @@ import org.mapstruct.MappingConstants;
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface BankMapper {
     BankStock toEntity(BankStockDTO dto);
+    BankStockDTO toDto(BankStock entity);
 }
