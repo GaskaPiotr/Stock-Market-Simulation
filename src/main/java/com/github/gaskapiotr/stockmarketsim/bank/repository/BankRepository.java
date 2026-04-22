10d0f7958cc4daef2da8ea86f9e9edfb5e2b984d
GaskaPiotr <04piotr04@wp.pl>
2026-04-22 13:58:34 +0200

    fet: add bank repo jpa repo extension

--- src/main/java/com/github/gaskapiotr/stockmarketsim/bank/repository/BankRepository.java
+++ src/main/java/com/github/gaskapiotr/stockmarketsim/bank/repository/BankRepository.java
@@ -1,7 +1,9 @@
 package com.github.gaskapiotr.stockmarketsim.bank.repository;
 
+import com.github.gaskapiotr.stockmarketsim.bank.entity.BankStock;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BankRepository {
+public interface BankRepository extends JpaRepository<BankStock, String> {
 }
