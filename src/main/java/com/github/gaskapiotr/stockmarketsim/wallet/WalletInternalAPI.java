8909fc8561eb0047653e3f8e0fa43fb5c253b13e
GaskaPiotr <04piotr04@wp.pl>
2026-04-22 14:01:57 +0200

    fix: delete wallet int api

--- src/main/java/com/github/gaskapiotr/stockmarketsim/wallet/WalletInternalAPI.java
+++
@@ -1,4 +0,0 @@
-package com.github.gaskapiotr.stockmarketsim.wallet;
-
-public interface WalletInternalAPI {
-}
--- src/main/java/com/github/gaskapiotr/stockmarketsim/wallet/manager/WalletManager.java
+++ src/main/java/com/github/gaskapiotr/stockmarketsim/wallet/manager/WalletManager.java
@@ -1,9 +1,8 @@
 package com.github.gaskapiotr.stockmarketsim.wallet.manager;
 
 import com.github.gaskapiotr.stockmarketsim.wallet.WalletExternalAPI;
-import com.github.gaskapiotr.stockmarketsim.wallet.WalletInternalAPI;
 import org.springframework.stereotype.Service;
 
 @Service
-public class WalletManager implements WalletExternalAPI, WalletInternalAPI {
+public class WalletManager implements WalletExternalAPI {
 }
