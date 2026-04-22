2795f029a6611d7b0582c20c59e7dd4caf14e3f7
GaskaPiotr <04piotr04@wp.pl>
2026-04-22 14:00:17 +0200

    chore: add bank ext api and implementation
    Adds empty bank ext api
    Also makes bank manager implement bank ext api

--- src/main/java/com/github/gaskapiotr/stockmarketsim/bank/manager/BankManager.java
+++ src/main/java/com/github/gaskapiotr/stockmarketsim/bank/manager/BankManager.java
@@ -1,7 +1,8 @@
 package com.github.gaskapiotr.stockmarketsim.bank.manager;
 
+import com.github.gaskapiotr.stockmarketsim.bank.BankExternalAPI;
 import org.springframework.stereotype.Service;
 
 @Service
-public class BankManager {
+public class BankManager implements BankExternalAPI {
 }
