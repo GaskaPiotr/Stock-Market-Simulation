374b055473747340064a76846e67b516719d01a2
GaskaPiotr <04piotr04@wp.pl>
2026-04-22 14:13:16 +0200

    chore: add framework for add stock
    Adds add stocks in ext api and empty add stocks in bank manager

--- src/main/java/com/github/gaskapiotr/stockmarketsim/bank/BankExternalAPI.java
+++ src/main/java/com/github/gaskapiotr/stockmarketsim/bank/BankExternalAPI.java
@@ -1,4 +1,7 @@
 package com.github.gaskapiotr.stockmarketsim.bank;
 
+import java.util.List;
+
 public interface BankExternalAPI {
+     void addStocks(List<BankStockDTO> stocks);
 }
--- src/main/java/com/github/gaskapiotr/stockmarketsim/bank/manager/BankManager.java
+++ src/main/java/com/github/gaskapiotr/stockmarketsim/bank/manager/BankManager.java
@@ -1,8 +1,20 @@
 package com.github.gaskapiotr.stockmarketsim.bank.manager;
 
 import com.github.gaskapiotr.stockmarketsim.bank.BankExternalAPI;
+import com.github.gaskapiotr.stockmarketsim.bank.BankStockDTO;
+import com.github.gaskapiotr.stockmarketsim.bank.repository.BankRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class BankManager implements BankExternalAPI {
+    private final BankRepository bankRepository;
+
+    @Override
+    public void addStocks(List<BankStockDTO> stocks) {
+        // TODO change dto to entity then add to repo
+    }
 }
