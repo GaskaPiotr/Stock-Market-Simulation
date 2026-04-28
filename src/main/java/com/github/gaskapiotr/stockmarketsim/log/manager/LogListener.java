45dea38b669230aed2d4e4aff40d4decd97c894e
GaskaPiotr <04piotr04@wp.pl>
2026-04-28 21:14:17 +0200

    fet: add log listener

--- src/main/java/com/github/gaskapiotr/stockmarketsim/log/manager/LogListener.java
+++ src/main/java/com/github/gaskapiotr/stockmarketsim/log/manager/LogListener.java
@@ -1,4 +1,23 @@
 package com.github.gaskapiotr.stockmarketsim.log.manager;
 
+import com.github.gaskapiotr.stockmarketsim.transaction.BuyStockEvent;
+import com.github.gaskapiotr.stockmarketsim.transaction.SellStockEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.modulith.events.ApplicationModuleListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class LogListener {
+    private final LogManager logManager;
+
+    @ApplicationModuleListener
+    void on(BuyStockEvent event) {
+        // TODO handle buy event
+    }
+
+    @ApplicationModuleListener
+    void on(SellStockEvent event) {
+        // TODO handle sell event
+    }
 }
