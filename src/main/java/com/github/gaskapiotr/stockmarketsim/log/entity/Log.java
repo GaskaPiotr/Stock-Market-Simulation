7618f6fea83a0d00995a97857656a94d4a319d54
GaskaPiotr <04piotr04@wp.pl>
2026-04-28 21:09:53 +0200

    fet: add log entity
    Adds log with id, type, wallet id and stock name

--- src/main/java/com/github/gaskapiotr/stockmarketsim/log/entity/Log.java
+++ src/main/java/com/github/gaskapiotr/stockmarketsim/log/entity/Log.java
@@ -1,7 +1,22 @@
 package com.github.gaskapiotr.stockmarketsim.log.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Log {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String type;
+    private String wallet_id;
+    private String stock_name;
 }
