ef3a6eb5cc4abbc69e9cecf0756f08fdc3c78a9b
GaskaPiotr <04piotr04@wp.pl>
2026-04-28 20:46:42 +0200

    fet: add get wallet stock quantity

--- src/main/java/com/github/gaskapiotr/stockmarketsim/wallet/WalletExternalAPI.java
+++ src/main/java/com/github/gaskapiotr/stockmarketsim/wallet/WalletExternalAPI.java
@@ -3,4 +3,5 @@ package com.github.gaskapiotr.stockmarketsim.wallet;
 public interface WalletExternalAPI {
     void sellStock(String wallet_id, String stock_name);
     WalletDTO getWallet(String wallet_id);
+    int getWalletStockQuantity(String wallet_id, String stock_name);
 }
