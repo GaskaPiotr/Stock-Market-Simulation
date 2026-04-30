f8e29eb4846ba8f6ee0936c55d448e9de21e891e
GaskaPiotr <04piotr04@wp.pl>
2026-04-30 17:44:34 +0200

    fet: add postgres as db
    Adds postgres in docker as db
    Also fixes log table name

--- src/main/java/com/github/gaskapiotr/stockmarketsim/log/entity/Log.java
+++ src/main/java/com/github/gaskapiotr/stockmarketsim/log/entity/Log.java
@@ -1,14 +1,12 @@
 package com.github.gaskapiotr.stockmarketsim.log.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@Table(name = "stock_log")
 @Getter
 @Setter
 @NoArgsConstructor
