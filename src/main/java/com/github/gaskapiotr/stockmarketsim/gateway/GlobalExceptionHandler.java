763fd8e0308c43e6a7e544c9934ce926d8047eca
GaskaPiotr <04piotr04@wp.pl>
2026-04-30 07:45:30 +0200

    fet: add global exception handler

---
+++ src/main/java/com/github/gaskapiotr/stockmarketsim/gateway/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.github.gaskapiotr.stockmarketsim.gateway;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+}
